7494110136d75524d0fc08b6b38bf6d6ca2599d0
Edwin Segura <seguraedwin5@gmail.com>
2024-04-28 17:46:41 -0500

    se agrega fondo de banner

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div>
+      <div className="banner absolute w-full h-full bg-[url('/public/rana_banner.jpeg')] bg-no-repeat bg-contain "></div>
+      <div className="relative text-center ">
+        <h2 className="text-white">Bienvenido</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
