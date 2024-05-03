bd846c7dd7a7f0957b577840672cb9d3102f288b
Edwin Segura <seguraedwin5@gmail.com>
2024-05-02 20:38:37 -0500

    ajuste merge Gabriel

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,50 +1,52 @@
-
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
 
-
-const { Brand } = Navbar
-const {Item}= Dropdown
+const { Brand } = Navbar;
+const { Item } = Dropdown;
 
 function NavbarComponent() {
   return (
     <Navbar fluid rounded>
-      <Brand><Link to={'/'}>
-        <img src="/logo.png" className="mr-3 h-9 sm:h-12" alt="Flowbite React Logo" /></Link>
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Bolirana</span>
+      <Brand>
+        <Link to={"/"}>
+          <img
+            src="/public/logo.png"
+            className="mr-3 h-9 sm:h-12"
+            alt="Flowbite React Logo"
+          />
+        </Link>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Bolirana
+        </span>
       </Brand>
       <div className="flex md:order-2">
         <h4>Login</h4>
       </div>
 
       <Navbar.Collapse>
-        <Navbar.Link active>Home</Navbar.Link>
-        <Navbar.Link><Link to='/'>Mas</Link></Navbar.Link>
-        <Navbar.Link><Link to='/'>Contacto</Link></Navbar.Link>
-        <Dropdown
-          arrowIcon={true}
-          inline
-          label={
-            'Servicios'
-          }
-        >
-          <Item><Link to='/campeonatos'>Campoeonato</Link></Item>
-          <Item><Link to='/eventos'>Eventos</Link></Item>
-          <Item><Link to='/alquiler'>Alquiler</Link></Item>
+        <Navbar.Link active>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/">Mas</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/">Contacto</Link>
+        </Navbar.Link>
+        <Dropdown arrowIcon={true} inline label={"Servicios"}>
+          <Item>
+            <Link to="/campeonatos">Campoeonato</Link>
+          </Item>
+          <Item>
+            <Link to="/eventos">Eventos</Link>
+          </Item>
+          <Item>
+            <Link to="/alquiler">Alquiler</Link>
+          </Item>
         </Dropdown>
         <Navbar.Toggle />
       </Navbar.Collapse>
     </Navbar>
-
-
-
-
-
-  )
+  );
 }
 export default NavbarComponent;
-
-
-
-
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import Section from "../components/Section";
 import Hero from "../components/Hero";
+import SectionCards from "../components/SectionCards";
 function Home() {
   return (
     <div>
       <Hero></Hero>
 
-      <Section>
-        <p>Este es un texto de seccion</p>
-      </Section>
+      <SectionCards></SectionCards>
     </div>
   );
 }
